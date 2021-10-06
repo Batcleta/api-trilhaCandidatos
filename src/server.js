0e6db2cdcdf0e7027f4eaaca20a435e3c49780ac
Batcleta <thewazig.wf@gmail.com>
2021-10-06 12:08:31 -0300

    Estrutura básica

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const Express = require('express');
+var cors = require('cors')
+const routes = require('./autoCand/routes/routes')
+
+require('./database')
+
+const app = Express()
+
+app.use(cors())
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
+app.use(routes)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
